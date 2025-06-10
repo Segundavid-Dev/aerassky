98cbfcb0b558a3db082343191ebe8d9d5e0b8148
Segundavid-Dev <segdavid03@gmail.com>
2025-06-10 21:46:14 +0100

    changed image path name

--- public/data.ts
+++ public/data.ts
@@ -32,3 +32,26 @@ export const reasonsArray: reasonsArrayType = [
     text: "(SEO) Mastery Boost your online visibility with our expert SEO techniques.",
   },
 ];
+
+export const servicesArray = [
+  {
+    img: "/web-dev-icon.png",
+    heading: "Web Development",
+    text: "Unlock Your Online Potential In today's digital age, a powerful web presence is essential for any business. At DigitX, our web development services empower you to stand out in the crowded online landscape. We create responsive and dynamic websites tailored to your unique needs, ensuring seamless user experiences across all devices. From e-commerce platforms to interactive web applications, our expert developers bring your vision to life, making your online journey a resounding success.",
+  },
+  {
+    img: "/Mobile-app-icon.png",
+    heading: "Mobile App Development",
+    text: "Embrace Mobility with Confidence Mobile devices have revolutionized the way we interact with the world. Our mobile app development services enable you to harness this mobility to your advantage. We design and build intuitive and high-performance mobile applications that captivate your audience and boost engagement. Whether it's iOS, Android, or cross-platform development, we ensure that your app delivers a seamless experience, leaving a lasting impression on your users.",
+  },
+  {
+    img: "/web-design-icon.png",
+    heading: "Web Design",
+    text: "Elevate Your Brand Aesthetics Your website's design is a reflection of your brand's identity and values. DigitX's web design services focus on creating visually striking and user-friendly interfaces that leave a lasting impact. Our creative team blends aesthetics with functionality, delivering a captivating user experience that keeps visitors coming back for more. Let us transform your online presence into an immersive journey that showcases your brand's true essence.",
+  },
+  {
+    img: "/Digitial-Marketing-icon.png",
+    heading: "Digital Marketing",
+    text: "Drive Your Business Forward In the vast digital landscape, standing out from the competition is crucial. Our digital marketing services help you rise above the noise and connect with your target audience effectively. From search engine optimization (SEO) to social media marketing and pay-per-click (PPC) campaigns, our data-driven strategies ensure that your message reaches the right people at the right time.",
+  },
+];
--- src/components/home/Services.tsx
+++ src/components/home/Services.tsx
@@ -1,15 +1,26 @@
 export default function Services() {
   return (
-    <div className="text-center">
-      <h2 className="text-[48px] font-semibold">
-        <span className="text-[var(--text-gray)]">Our</span> Services
-      </h2>
-      <p className="text-[var(--text-gray)] pb-5 w-[70%] mx-auto">
-        Our comprehensive range of services includes web design, mobile app
-        development, SEO, social media marketing, and more. Whether you're a
-        startup or an established enterprise, our experts will craft solutions
-        that drive results.
-      </p>
+    <div>
+      <div className="text-center">
+        <h2 className="text-[48px] font-semibold">
+          <span className="text-[var(--text-gray)]">Our</span> Services
+        </h2>
+        <p className="text-[var(--text-gray)] pb-10 w-[70%] mx-auto">
+          Our comprehensive range of services includes web design, mobile app
+          development, SEO, social media marketing, and more. Whether you're a
+          startup or an established enterprise, our experts will craft solutions
+          that drive results.
+        </p>
+      </div>
+      <ServicesGrid />
+    </div>
+  );
+}
+
+function ServicesGrid() {
+  return (
+    <div>
+      <p>services grid</p>
     </div>
   );
 }
