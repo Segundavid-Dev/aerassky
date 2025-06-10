3bf6f3b138c2ff9a44f4b4a0f442661164b8e8f3
Segundavid-Dev <segdavid03@gmail.com>
2025-06-10 10:56:29 +0100

    restructure projects files and folder to maintain reusabiltity and single responsibility principle in react

---
+++ public/data.ts
@@ -0,0 +1,34 @@
+import type { reasonsArrayType } from "../src/types/index";
+
+export const reasonsArray: reasonsArrayType = [
+  {
+    img: "/images/expertise.png",
+    heading: "Expertise That Drives Results",
+    text: "Our team of seasoned professionals brings years of experience and expertise to the table.",
+  },
+  {
+    img: "/images/web-design.png",
+    heading: "Tailored Business Solutions ",
+    text: "We understand that every business is unique. That's why our solutions are customized.",
+  },
+  {
+    img: "/images/expertise.png",
+    heading: "Cutting-Edge Web Design",
+    text: "Leave a lasting impression on your audience with our top-notch web design services.",
+  },
+  {
+    img: "/images/mobile.png",
+    heading: "Mobile-First Approach",
+    text: "In today's mobile-centric world, we prioritize mobile-first design to ensure your website.",
+  },
+  {
+    img: "/images/marketing-strategies.png",
+    heading: "Marketing Strategies",
+    text: "Our data-driven marketing strategies allow us to target the right audience with precision",
+  },
+  {
+    img: "/images/seo.png",
+    heading: "Search Engine Optimization",
+    text: "(SEO) Mastery Boost your online visibility with our expert SEO techniques.",
+  },
+];
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,9 @@ export type ButtonType = {
   className?: string;
   children: React.ReactNode;
 };
+
+export type reasonsArrayType = {
+  img: string;
+  heading: string;
+  text: string;
+}[];
