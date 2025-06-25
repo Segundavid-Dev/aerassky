e1faf96c59767f19e603982c9f702a53a324a907
Segundavid-Dev <segdavid03@gmail.com>
2025-06-25 14:18:40 +0100

    update nav to handle responsiveness

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,5 +1,9 @@
 import { NavLink } from "react-router";
 
+import { Menu } from "lucide-react";
+
+import { useState } from "react";
+
 const navLinks = [
   {
     section: "Home",
@@ -32,15 +36,30 @@ const navLinks = [
 ];
 
 export default function HomeNav() {
+  const [navOpen, setnavOpen] = useState<boolean>(false);
+
+  const handleNavOpen = () => {
+    setnavOpen((navOpen) => !navOpen);
+  };
   return (
-    <nav>
-      <ul className="flex gap-8 items-center justify-center py-7">
+    <nav className="flex items-center justify-between py-10">
+      <div className="z-20">
+        <img src="/images/home/aeraskyy-logo.png" alt="" />
+      </div>
+      <ul
+        className={`flex gap-8 items-center justify-center max-sm:flex-col max-sm:fixed max-sm:left-0 ${
+          navOpen ? "top-0" : "max-sm:top-[-100%]"
+        } max-sm:w-full max-sm:h-screen max-sm:bg-black/90 duration-300 transition-all`}
+      >
         {navLinks.map((item, index) => (
           <li key={index} className="cursor-pointer">
             <NavLink to={item.link}>{item.section}</NavLink>
           </li>
         ))}
       </ul>
+      <div className="hidden max-sm:block z-20" onClick={handleNavOpen}>
+        <Menu />
+      </div>
     </nav>
   );
 }
